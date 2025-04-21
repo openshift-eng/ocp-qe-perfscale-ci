b576e993861aff571d06fb218ed8a1f7f849c269
memodi <memodi@redhat.com>
2025-04-21 15:51:39 -0400

    Create and share spreadsheets

--- scripts/sheets/sheets.py
+++ scripts/sheets/sheets.py
@@ -1,26 +1,86 @@
 import google.oauth2.service_account
 import apiclient.discovery
 from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+import logging
+
+logger = logging.getLogger("perfsheets")
 
 
 class Sheets:
-    _Scopes_Read_Only = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    _Scopes_Read_Write = ["https://www.googleapis.com/auth/spreadsheets"]
-
     def __init__(self, sheetid, cell_range, service_account_file):
         self.sheetid = sheetid
         self.service_account = service_account_file
         self.cell_range = cell_range
 
     def _get_sheets_service(self, read_only: bool = False):
-        scopes = Sheets._Scopes_Read_Only if read_only else Sheets._Scopes_Read_Write
-        credentials = (
+        scopes = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        self.credentials = (
             google.oauth2.service_account.Credentials.from_service_account_file(
                 str(self.service_account),
                 scopes=scopes,
             )
         )
-        return apiclient.discovery.build("sheets", "v4", credentials=credentials)
+        return apiclient.discovery.build("sheets", "v4", credentials=self.credentials)
+
+    def create(self, title):
+        """Create a spreadsheet with title"""
+        spreadsheet = {"properties": {"title": title}}
+        try:
+            spreadsheet = (
+                self._get_sheets_service()
+                .spreadsheets()
+                .create(body=spreadsheet, fields="spreadsheetId")
+                .execute()
+            )
+            logger.info(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+            self.sheetid = spreadsheet.get("spreadsheetId")
+        except HttpError as error:
+            raise Exception(f"An error occurred while creating google sheet: {error}")
+
+    def create_permission(self, email):
+        """Adds write permissions for email user to the generated sheet"""
+
+        permission = {
+            "type": "user",
+            "role": "writer",
+            "emailAddress": email,
+        }
+
+        service = apiclient.discovery.build("drive", "v3", credentials=self.credentials)
+        try:
+            service.permissions().create(fileId=self.sheetid, body=permission).execute()
+        except HttpError as error:
+            raise Exception(
+                f"An error occurred while adding permissions to google sheet: {error}"
+            )
+
+    def auto_resize_columns(self):
+        """Resizes columns in a Google Sheet to fit the data."""
+        request_body = {
+            "requests": [
+                {
+                    "autoResizeDimensions": {
+                        "dimensions": {
+                            "sheetId": 0,
+                            "dimension": "COLUMNS",
+                            "startIndex": 0,
+                            "endIndex": 7,
+                        }
+                    }
+                }
+            ]
+        }
+        try:
+            self._get_sheets_service().spreadsheets().batchUpdate(
+                spreadsheetId=self.sheetid, body=request_body
+            ).execute()
+        except HttpError as error:
+            raise Exception(f"Resizing columns failed: {error}")
 
     def get_values(self) -> list[list]:
         """
